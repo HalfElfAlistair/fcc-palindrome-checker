461da7955ca296ad5cba3a7bde44ea5b79e4c488
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-01-08 09:05:37 +0000

    added HTML and amended JS to hook it up

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,28 +1,31 @@
-function palindrome(str) {
+function palindromeCheck() {
+  let str = document.getElementById("string").value;
   let newArray = [];
   let arrayToCheck = str.toLowerCase().split("");
 
 
-for (let i = 0; i < arrayToCheck.length; i++) {
-  if ((/\w/.test(arrayToCheck[i])) && !(/_/.test(arrayToCheck[i]))) {
-    newArray.push(arrayToCheck[i]);
-  }
-}
-
-let combinedString = newArray.join("");
-let mirrorPoint = combinedString.length / 2;
-let firstHalf = combinedString.slice(0, mirrorPoint)
-let secondHalf = combinedString.slice(mirrorPoint)
-
-if (firstHalf.length < secondHalf.length) {
-    secondHalf = secondHalf.slice(1).split("").reverse().join("");
-  } else if (firstHalf.length === secondHalf.length) {
-    secondHalf = secondHalf.split("").reverse().join("");
+  for (let i = 0; i < arrayToCheck.length; i++) {
+    if ((/\w/.test(arrayToCheck[i])) && !(/_/.test(arrayToCheck[i]))) {
+      newArray.push(arrayToCheck[i]);
+    }
   }
 
-    if (firstHalf === secondHalf) {
-    return true;
-  }
+  let combinedString = newArray.join("");
+  let mirrorPoint = combinedString.length / 2;
+  let firstHalf = combinedString.slice(0, mirrorPoint);
+  let secondHalf = combinedString.slice(mirrorPoint);
 
-  return false;
+  if (firstHalf.length < secondHalf.length) {
+      secondHalf = secondHalf.slice(1).split("").reverse().join("");
+    } else if (firstHalf.length === secondHalf.length) {
+      secondHalf = secondHalf.split("").reverse().join("");
+    }
+
+      if (firstHalf === secondHalf) {
+        document.getElementById("palindrome").innerHTML = "Yes, that's a palindrome!";
+    } else {
+      document.getElementById("palindrome").innerHTML = "Sorry, that's not a palindrome.";
+    }
+
+    
 }
